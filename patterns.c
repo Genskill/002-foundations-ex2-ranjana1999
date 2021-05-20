d15eca68488a58e7b4e9cfe1fd4a06a1b0749f26
ranjana1999 <52955034+ranjana1999@users.noreply.github.com>
2021-05-20 20:48:54 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -6,12 +6,12 @@ void pyramid_s(int);
 int main(void){
 	int ch = get_int("Enter type of pattern (1 or 2) ");
 	if (ch == 1){
-		int row = get_int("Number of rows\n");
+		int row = get_int("Number of rows");
 		pyramid_f(row);
 	}
 	else{
 		if(ch == 2){
-			int row = get_int("Number of rows\n");
+			int row = get_int("Number of rows");
 			pyramid_s(row);
 			}
 		}
